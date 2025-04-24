dbd7ad42f32e29c564c189bf387680a75b0654fb
Chris Butler <chris.butler@redhat.com>
2025-04-24 14:25:05 +1000

    chore: add license
    Signed-off-by: Chris Butler <chris.butler@redhat.com>

--- src/lightroom_2_darktable/__about__.py
+++ src/lightroom_2_darktable/__about__.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2025-present Chris Butler <chris.butler@redhat.com>
+# SPDX-FileCopyrightText: 2025-present Chris Butler <chris@thebutlers.me>
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: Apache-2.0
 __version__ = "0.0.1"
