3daafba69fadf5e1ca6defa4ae32edda82ff3f5c
Chris Butler <chris.butler@redhat.com>
2025-04-24 20:08:52 +1000

    feat: add cli skeleton
    Signed-off-by: Chris Butler <chris.butler@redhat.com>

---
+++ src/lightroom_2_darktable/cli/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2025-present Chris Butler <chris@thebutlers.me>
+#
+# SPDX-License-Identifier: Apache-2.0
+"""Core CLI init using tyber"""
+
+
+
+def cli():
+    print('hello')
